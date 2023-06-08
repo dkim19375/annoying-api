5666fe240b15c970ecbd85c845f46c8fcbfd1792
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2023-06-08 18:35:34 -0400

    test

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -24,7 +24,7 @@ java {
 configure<PublishingExtension> {
     publications {
         create<MavenPublication>("maven") {
-            groupId = "com.github.srnyx"
+            groupId = "com.github.dkim19375.api"
             artifact(tasks["shadowJar"])
             artifact(tasks["javadocJar"])
         }
